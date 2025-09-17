ad2b6fcab9f6358c2338dc7508916e5fca8e39cf
Syed Kaab Surkhi <ssurkhi56@gmail.com>
2025-09-16 23:44:46 -0400

    Refactor Supabase client configuration
    Replaced hardcoded Supabase URL and anon key with variables.

--- frontend_site/src/helper/supabaseClient.js
+++ frontend_site/src/helper/supabaseClient.js
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseURL = "https://cswqvcabjksyafhzkrew.supabase.co";
-const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNzd3F2Y2FiamtzeWFmaHprcmV3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTY2MDExMzMsImV4cCI6MjA3MjE3NzEzM30.p0gUVztzBvVqRqy1FL5k2fIIhizy8bbkaY6BeWc7IWQ"
+const supabaseURL = [supabaseUrl];
+const supabaseAnonKey = [supabaseAnonKey];
 
 const supabase = createClient(supabaseURL, supabaseAnonKey);
 
